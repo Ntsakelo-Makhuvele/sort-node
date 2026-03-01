9a2396a59e1cb02ee4c6a98b99f2ca59061aac9c
Ntsakelo <ntsakycodes@gmail.com>
2026-03-01 20:43:04 +0200

    add error handling

--- src/controllers/sort.js
+++ src/controllers/sort.js
@@ -8,8 +8,14 @@ export const sortController = (req,res) => {
             res.status(200).json({
                 word:sorted
             })
+          }else{
+            res.status(400).json({
+                message:'something went wrong'
+            })
           } 
     }catch(err){
-
-    }
+        res.status(500).json({
+            message: 'there was an internal error'
+        })
+    } 
 }
